19d6451ff22864c62093bda9bbbb3e72c818c5af
Ilya <ilyastarickov1996@gmail.com>
2021-06-26 16:02:45 +0400

    add: fix deploy failure

--- src/ts/fuzzy-searchFunCreator/fuzzy-searchFunCreator.ts
+++ src/ts/fuzzy-searchFunCreator/fuzzy-searchFunCreator.ts
@@ -1,6 +1,5 @@
 import { EnhancedStore } from "@reduxjs/toolkit";
 import FuzzySearch from "fuzzy-search";
-import "@types/fuzzy-search";
 
 export function fuzzySearchCreator(store: EnhancedStore): FuzzySearch<Task[]> {
   return new FuzzySearch(store.getState(), ["description"], {
